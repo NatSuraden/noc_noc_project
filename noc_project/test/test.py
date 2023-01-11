c9ff29f5ae9c0f0f8286808c5573c61606b00205
Suradet Loedwilaisap <nat037222161@hotmail.com>
2023-01-11 11:34:50 +0700

    1

--- noc_project/test/test.py
+++ noc_project/test/test.py
@@ -39,39 +39,57 @@ cursor.execute('SELECT * FROM equipment')
 equipment = cursor.fetchall()
 cursor.execute('SELECT * FROM interface')
 interface = cursor.fetchall()
-a = "(['V53802', 'Makro', 'ลาดกระบัง', 'FGT60FTK20054815', '10.11.220.8', '10.99.178.18'], 'V53802')"
+cursor.execute('SELECT * FROM project')
+project = cursor.fetchall()
+cursor.execute('SELECT * FROM contract')
+contrat = cursor.fetchall()
+# a = "(['V53802', 'Makro', 'ลาดกระบัง', 'FGT60FTK20054815', '10.11.220.8', '10.99.178.18'], 'V53802')"
+a = "(['V53802', 'Makro', 'ลาดกระบัง', 'FGT60FTK20054815', '10.11.220.8', '10.99.178.18'], 'Makro')"
 res = ast.literal_eval(a)
 print(res)
 print(res[1])
 index = res[0].index(res[1])
 print(index)
-
-if index == 0: #circuit_ID
-    zone1 = [res[0][1]] #Project Name
-    zone2 = [] #Detial circuit ทั้งหมด
-    zone3 = ["","",""] #Equipment Model ,Equipment Brand ,Serial_numbe
-    zone4 = [] #Physical Interface , VLAN_ID , Tunnel Interface Name
-    for i in circuit:
+if index == 1:
+    zone1 = [res[1],res[0][0],res[0][3]] #Project Name , circuit_Id ,Serial_numbe 
+    zone2 = [] #project detials
+    zone3 = [] #contrat
+    for i in project:
         if i[0] == res[1]:
             zone2 = i
-            zone3[-1] = i[1]
             break
-    for i in equipment:
-        if i[0] == zone3[-1]:
-            zone3[0] = i[4]
-            zone3[1] = i[3]
-            break
-    for i in interface:
+    #print(zone2)
+    for i in contrat:
         if i[1] == res[1]:
-            zone4.append(i[-3])
-            zone4.append(i[-2])
-            zone4.append(i[-1])
-    print(zone1)
-    print(zone2)
+            zone3 = i
+            break
     print(zone3)
-    print(zone4)
+# if index == 0: #circuit_ID
+#     zone1 = [res[0][1]] #Project Name
+#     zone2 = [] #Detial circuit ทั้งหมด
+#     zone3 = ["","",""] #Equipment Model ,Equipment Brand ,Serial_numbe
+#     zone4 = [] #Physical Interface , VLAN_ID , Tunnel Interface Name
+#     for i in circuit:
+#         if i[0] == res[1]:
+#             zone2 = i
+#             zone3[-1] = i[1]
+#             break
+#     for i in equipment:
+#         if i[0] == zone3[-1]:
+#             zone3[0] = i[4]
+#             zone3[1] = i[3]
+#             break
+#     for i in interface:
+#         if i[1] == res[1]:
+#             zone4.append(i[-3])
+#             zone4.append(i[-2])
+#             zone4.append(i[-1])
+#     print(zone1)
+#     print(zone2)
+#     print(zone3)
+#     print(zone4)
 
-    print(zone4[-1])
-    zone4[-1] = str(zone4[-1]).replace("\n"," <br /> ")
-    print(zone4)
+#     print(zone4[-1])
+#     zone4[-1] = str(zone4[-1]).replace("\n"," <br /> ")
+#     print(zone4)
     
