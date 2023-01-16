178a4c4a17dab5ca4605708ee51a3dc88c3537ae
Suradet Loedwilaisap <nat037222161@hotmail.com>
2023-01-16 16:14:41 +0700

    Test advance search v1

--- noc_project/test/advance_search.py
+++ noc_project/test/advance_search.py
@@ -181,11 +181,86 @@ for i in range(19):
                     pass
                 else:
                     equipment_search.append(data_in_process)
+    elif i == 15:
+        for n in circuit:
+            data_in_process = []
+            #print(n)
+            if str(n[0]).upper() == str(inputdata[15]).upper():
+                data_in_process.append(n[0]) #circuit_id  added
+                for a in equipment:
+                    if n[1] == a[0]:
+                        data_in_process.append(a[1]) #project_name added
+                        data_in_process.append(a[2]) #site_name added
+                        data_in_process.append(a[0]) #serial_number added
+                        break
+                data_in_process.append(n[5])     #Equipment_Loopback  added
+                data_in_process.append(n[3])   #IP_address_CE  added
+                circuit_search.append(data_in_process)
+                break
+    elif i == 16:
+      for n in circuit:
+            data_in_process = []
+            #print(n)
+            if str(n[3]).upper() == str(inputdata[16]).upper():
+                data_in_process.append(n[0]) #circuit_id  added
+                for a in equipment:
+                    if n[1] == a[0]:
+                        data_in_process.append(a[1]) #project_name added
+                        data_in_process.append(a[2]) #site_name added
+                        data_in_process.append(a[0]) #serial_number added
+                        break
+                data_in_process.append(n[5])     #Equipment_Loopback  added
+                data_in_process.append(n[3])   #IP_address_CE  addedd
+                if data_in_process in circuit_search:
+                    pass
+                else:
+                    circuit_search.append(data_in_process)
+                    break 
+    elif i == 17:
+      for n in circuit:
+            data_in_process = []
+            #print(n)
+            if str(n[5]).upper() == str(inputdata[17]).upper():
+                data_in_process.append(n[0]) #circuit_id  added
+                for a in equipment:
+                    if n[1] == a[0]:
+                        data_in_process.append(a[1]) #project_name added
+                        data_in_process.append(a[2]) #site_name added
+                        data_in_process.append(a[0]) #serial_number added
+                        break
+                data_in_process.append(n[5])     #Equipment_Loopback  added
+                data_in_process.append(n[3])   #IP_address_CE  addedd
+                if data_in_process in circuit_search:
+                    pass
+                else:
+                    circuit_search.append(data_in_process)
+                    break
+    elif i == 18:
+      for n in circuit:
+            data_in_process = []
+            #print(n)
+            if str(n[-2]).upper() == str(inputdata[18]).upper():
+                data_in_process.append(n[0]) #circuit_id  added
+                for a in equipment:
+                    if n[1] == a[0]:
+                        data_in_process.append(a[1]) #project_name added
+                        data_in_process.append(a[2]) #site_name added
+                        data_in_process.append(a[0]) #serial_number added
+                        break
+                data_in_process.append(n[5])     #Equipment_Loopback  added
+                data_in_process.append(n[3])   #IP_address_CE  addedd
+                if data_in_process in circuit_search:
+                    pass
+                else:
+                    circuit_search.append(data_in_process)
 print("xxxxxxxxxxxxxxxxxxx")
 for i in project_search:
     print(i)
 print("xxxxxxxxxxxxxxxxxxx")
 for i in site_search:
+    print(i)
+print("xxxxxxxxxxxxxxxxxxx")
+for i in circuit_search:
     print(i)
 print("xxxxxxxxxxxxxxxxxxx")
 for i in equipment_search:
